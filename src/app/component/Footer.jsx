ef90215e74ae0602e93ac650ac52592b6ceb9b2b
Codiemaureen <codiestover.cs@gmail.com>
2025-04-09 14:06:04 -0400

    Style: adding footer container

---
+++ src/app/component/Footer.jsx
@@ -0,0 +1,11 @@
+import style from '../component/Footer.module.css';
+
+const Footer = () => {
+  return (
+    <div className={style.footerContainer}>
+      <p>footer</p>
+    </div>
+  )
+}
+
+export default Footer
