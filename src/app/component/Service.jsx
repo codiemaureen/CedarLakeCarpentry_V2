1f2ec08ec808ddcde1c80491728155f6ed5bb10e
Codiemaureen <codiestover.cs@gmail.com>
2025-04-09 13:52:49 -0400

    Style: adding service components

---
+++ src/app/component/Service.jsx
@@ -0,0 +1,11 @@
+import style from '../component/Service.module.css';
+
+const Service = () => {
+  return (
+    <div className={style.idvService}>
+      <p>service</p>
+    </div>
+  )
+}
+
+export default Service
--- src/app/page.js
+++ src/app/page.js
@@ -1,11 +1,14 @@
 import Image from "next/image";
 import styles from "./page.module.css";
 import Header from "./component/Header";
+import ServiceContainer from "./component/ServiceContainer";
+
 
 export default function Home() {
   return (
     <div className={styles.page}>
       <Header/>
+      <ServiceContainer />
     </div>
   );
 }
