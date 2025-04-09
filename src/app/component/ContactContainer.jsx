c39ff35e6211cfa54aadfdab456d435e0c257d7f
Codiemaureen <codiestover.cs@gmail.com>
2025-04-09 14:44:35 -0400

    Adding contact form

--- src/app/component/ContactContainer.jsx
+++ src/app/component/ContactContainer.jsx
@@ -1,9 +1,11 @@
 import style from '../component/ContactContainer.module.css';
+import ContactForm from './ContactForm';
 
 const ContactContainer = () => {
   return (
     <div className={style.contactContainer}>
       <p>Contact Container</p>
+      <ContactForm />
     </div>
   )
 }
---
+++ src/app/component/ContactForm.jsx
@@ -0,0 +1,11 @@
+import style from '../component/ContactForm.module.css';
+
+const ContactForm = () => {
+  return (
+    <div className={style.contactFormContainer}>
+      <p>form</p>
+    </div>
+  )
+}
+
+export default ContactForm
