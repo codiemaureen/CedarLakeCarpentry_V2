7e45f4aebbe3854fbf87f81c267bf9243cb0d4fa
Codiemaureen <codiestover.cs@gmail.com>
2025-04-10 19:01:15 -0400

    Feat: Adding service components

--- src/app/component/Service.jsx
+++ src/app/component/Service.jsx
@@ -1,9 +1,11 @@
 import style from '../component/Service.module.css';
 
-const Service = () => {
+const Service = ({service}) => {
   return (
     <div className={style.idvService}>
-      <p>service</p>
+      <h3>{service.title}</h3>
+      <img src={service.img} alt={service.title} />
+      <p>{service.description}</p>
     </div>
   )
 }
--- src/app/component/ServiceContainer.jsx
+++ src/app/component/ServiceContainer.jsx
@@ -1,11 +1,27 @@
+"use client";
 import style from '../component/ServiceContainer.module.css';
 import Service from './Service';
+import { useState, useEffect } from 'react';
 
 const ServiceContainer = () => {
+  const [services, setServices] = useState([]);
+
+  useEffect(() => {
+    fetch("/services.json")
+    .then((response) => response.json())
+    .then((data) => setServices(data.services))      
+    .catch((error) => console.error("Error fetching services:", error));
+  }, []);
   return (
     <div className={style.serviceContainer}>
       <p>Services</p>
-      <Service/>
+      <div>
+      {services.map((service) => (
+        <div key={service.id}>
+          <Service service={service}/>
+        </div>
+      ))}
+      </div>
     </div>
   )
 }
