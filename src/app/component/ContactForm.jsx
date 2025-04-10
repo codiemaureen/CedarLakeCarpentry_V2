43e31d3f0e48f72a498973cb2253be3a05d71b67
Codiemaureen <codiestover.cs@gmail.com>
2025-04-10 19:28:07 -0400

    Style: adding form layout

--- src/app/component/ContactForm.jsx
+++ src/app/component/ContactForm.jsx
@@ -3,7 +3,20 @@ import style from '../component/ContactForm.module.css';
 const ContactForm = () => {
   return (
     <div className={style.contactFormContainer}>
-      <p>form</p>
+      <form action="">
+        <div className={style.emailInput}>
+          <label htmlFor='email'>Email</label>
+          <input type='text' placeholder='Enter your email....'/>
+        </div>
+        <div className={style.subjectInput}>
+          <label htmlFor='subject'>Enter your Subject</label>
+          <input type='text' placeholder='Enter Your subject'/>
+        </div>
+        <div className={style.messageInput}>
+          <label htmlFor='message'>Enter Message</label>
+          <textarea placeholder='enter your message'></textarea>
+        </div>
+      </form>
     </div>
   )
 }
