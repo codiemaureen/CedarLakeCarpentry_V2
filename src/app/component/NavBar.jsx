e1295bee6803174e2a3d8b68070a9582faa2196b
Codiemaureen <codiestover.cs@gmail.com>
2025-04-10 19:47:22 -0400

    Style: adding navbar container

---
+++ src/app/component/NavBar.jsx
@@ -0,0 +1,11 @@
+import style from '../component/NavBar.module.css';
+
+const NavBar = () => {
+  return (
+    <nav className={style.navBarContainer}>
+      <p>navbar</p>
+    </nav>
+  )
+}
+
+export default NavBar
