bb7ec049274e02329efc328904abfa98175b847e
Codiemaureen <codiestover.cs@gmail.com>
2025-04-10 17:45:49 -0400

    Style: adding photo cred for header photo

--- src/app/component/Footer.jsx
+++ src/app/component/Footer.jsx
@@ -4,6 +4,7 @@ const Footer = () => {
   return (
     <div className={style.footerContainer}>
       <p>footer</p>
+      <p>Header Image by Photo by <a href='https://www.pexels.com/photo/carpenter-using-circular-saw-for-precise-woodcutting-28518832/' alt='header-photo-cred'>Mario</a></p>
     </div>
   )
 }
