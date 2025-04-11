e5b8c649cd97573493b24ee9a50d8807df352ef8
Codiemaureen <codiestover.cs@gmail.com>
2025-04-11 11:01:03 -0400

    Styling: fixing footer position

--- src/app/component/Footer.jsx
+++ src/app/component/Footer.jsx
@@ -5,6 +5,7 @@ const Footer = () => {
     <div className={style.footerContainer}>
       <p>footer</p>
       <p>Header Image by Photo by <a href='https://www.pexels.com/photo/carpenter-using-circular-saw-for-precise-woodcutting-28518832/' alt='header-photo-cred'>Mario</a></p>
+      <p>Services Images by <a href='https://www.pexels.com/photo/couch-with-blue-pillows-in-a-living-room-5178068/'>Curtis</a></p>
     </div>
   )
 }
