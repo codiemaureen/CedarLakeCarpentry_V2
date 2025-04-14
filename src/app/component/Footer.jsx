97a733c888854e9095e0cf05cf301d7ed6e1ae36
Codiemaureen <codiestover.cs@gmail.com>
2025-04-14 13:26:22 -0400

    Style: adding color and style to footer

--- src/app/component/Footer.jsx
+++ src/app/component/Footer.jsx
@@ -3,7 +3,7 @@ import style from '../component/Footer.module.css';
 const Footer = () => {
   return (
     <div className={style.footerContainer}>
-      <p>footer</p>
+      <p>Website built by <a href="https://codiemaureen.netlify.app/">Codie Groth</a></p>
       <p>Header Image by Photo by <a href='https://www.pexels.com/photo/carpenter-using-circular-saw-for-precise-woodcutting-28518832/' alt='header-photo-cred'>Mario</a></p>
       <p>Services Images by <a href='https://www.pexels.com/photo/couch-with-blue-pillows-in-a-living-room-5178068/'>Curtis</a></p>
     </div>
