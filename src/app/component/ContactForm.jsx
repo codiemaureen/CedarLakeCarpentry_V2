10468d4bfc0b242d6f6c8a01a165e7ee0e8e2e63
Codiemaureen <codiestover.cs@gmail.com>
2025-04-14 13:16:18 -0400

    Style: adding contactForm Syle

--- src/app/component/ContactForm.jsx
+++ src/app/component/ContactForm.jsx
@@ -3,18 +3,21 @@ import style from '../component/ContactForm.module.css';
 const ContactForm = () => {
   return (
     <div className={style.contactFormContainer}>
-      <form action="">
-        <div className={style.emailInput}>
-          <label htmlFor='email'>Email</label>
-          <input type='text' placeholder='Enter your email....'/>
+      <form action="" className={style.ContactForm}>
+        <div className={style.inputContainer}>
+          <label htmlFor='email'>Enter your Email:</label>
+          <input type='text' 
+          className={style.inputBox}placeholder='Enter your email....'/>
         </div>
-        <div className={style.subjectInput}>
-          <label htmlFor='subject'>Enter your Subject</label>
-          <input type='text' placeholder='Enter Your subject'/>
+        <div className={style.inputContainer}>
+          <label htmlFor='subject'>Enter your Subject:</label>
+          <input type='text'
+          className={style.inputBox} placeholder='Enter Your subject'/>
         </div>
-        <div className={style.messageInput}>
-          <label htmlFor='message'>Enter Message</label>
-          <textarea placeholder='enter your message'></textarea>
+        <div className={style.inputContainer}>
+          <label htmlFor='message'>Enter Message:</label>
+          <textarea 
+          className={style.inputBox}placeholder='enter your message'></textarea>
         </div>
       </form>
     </div>
