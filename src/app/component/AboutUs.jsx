076e8c78c3e37bd13d9a7961c2d205ffbc27eec6
Codiemaureen <codiestover.cs@gmail.com>
2025-04-14 12:05:04 -0400

    Style: Adding size, color, and flexibility to component

--- src/app/component/AboutUs.jsx
+++ src/app/component/AboutUs.jsx
@@ -3,8 +3,8 @@ import style from '../component/AboutUs.module.css';
 const AboutUs = () => {
   return (
     <div className={style.aboutUsContainer}>
-      <h2>About Us</h2>
-      <p>With over 100 years of combined experience, Cedar Lake Carpentry is the premier choice for luxury finished carpentry in the Greater Detroit area. We specialize in elevating new homes with exquisite trim work, custom built-ins, crown molding, and architectural detailing. Our team is dedicated to precision, elegance, and timeless craftsmanship—bringing refined beauty to every space we touch.</p>
+      <h2 className={style.aboutHeader}>About Us</h2>
+      <p className={style.aboutDesc}>With over 100 years of combined experience, Cedar Lake Carpentry is the premier choice for luxury finished carpentry in the Greater Detroit area. We specialize in elevating new homes with exquisite trim work, custom built-ins, crown molding, and architectural detailing. Our team is dedicated to precision, elegance, and timeless craftsmanship—bringing refined beauty to every space we touch.</p>
     </div>
   )
 }
