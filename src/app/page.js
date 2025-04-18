e79dc54f2ed506f2f082d599b1d830b7a1fdbf05
Codiemaureen <codiestover.cs@gmail.com>
2025-04-18 12:06:41 -0400

    Feature: adding navBar

--- src/app/page.js
+++ src/app/page.js
@@ -5,11 +5,13 @@ import ServiceContainer from "./component/ServiceContainer";
 import Footer from "./component/Footer";
 import AboutUs from "./component/AboutUs";
 import ContactContainer from "./component/ContactContainer";
+import NavBar from "./component/NavBar";
 
 
 export default function Home() {
   return (
     <div className={styles.page}>
+      <NavBar/>
       <Header/>
       <ServiceContainer />
       <AboutUs />
