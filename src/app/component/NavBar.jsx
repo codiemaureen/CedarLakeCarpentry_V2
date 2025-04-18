3edcf6fed5c6f053e80a595cf90d824325b519c4
Codiemaureen <codiestover.cs@gmail.com>
2025-04-18 12:20:49 -0400

    Feature: adding navbar logic and style

--- src/app/component/NavBar.jsx
+++ src/app/component/NavBar.jsx
@@ -1,9 +1,20 @@
+import Link from 'next/link';
 import style from '../component/NavBar.module.css';
 
 const NavBar = () => {
+    let navItems = [
+    {id: 0, title:'home', link: ''}, 
+    {id: 1, title:'about', link:"#aboutUsContainer" } , 
+    {id: 2, title: 'services', link: "#servicesContainer" }, ,
+    {id: 3, title: 'Get a Quote', link: '' }
+  ]
   return (
     <nav className={style.navBarContainer}>
-      <p>navbar</p>
+      {navItems.map((items) => (
+        <ul key={items.id} className={style.navBar}>
+          <li  className={style.navItem}><Link href={items.link}>{items.title}</Link></li>
+        </ul>
+      ))}
     </nav>
   )
 }
