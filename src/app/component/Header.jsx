527e89fa8876176165a864dd26af84d27498c245
Codiemaureen <codiestover.cs@gmail.com>
2025-04-22 18:38:01 -0400

    Style: changing font, color, and mobile view

--- src/app/component/Header.jsx
+++ src/app/component/Header.jsx
@@ -1,10 +1,16 @@
 import style from '../component/Header.module.css';
+import { Bevan, Inter } from 'next/font/google';
 
+const bevan = Bevan({
+  weight: '400',
+  subsets: ['latin'],
+  variable: '--font-bevan',
+})
 
 const Header = () => {
   return (
     <div className={style.headerContainer}>
-      <h1 className={style.headerTitle}>Cedar Lake Carpentry</h1>
+      <h1 className={`${style.headerTitle} ${bevan.className}`}>Cedar Lake Carpentry</h1>
       <img src='pexels-spencphoto-28518832.jpg' alt='header-img' className={style.headerImg}/>
     </div>
   )
