28b1af8137ead98e8f7c9798647e781956dab9a1
Codiemaureen <codiestover.cs@gmail.com>
2025-04-28 11:08:43 -0400

    Style: adding services description, adjust mobile view to support longer descriptions

--- src/app/component/ServiceContainer.jsx
+++ src/app/component/ServiceContainer.jsx
@@ -15,6 +15,7 @@ const ServiceContainer = () => {
   return (
     <div id='serviceContainer' className={style.serviceContainer}>
       <h2>Services</h2>
+      <p className={style.servicesDesc}>At Cedar Lake Carpentry, craftsmanship isn’t just a service — it’s an art. We bring unmatched skill, attention to detail, and luxury craftsmanship to every project, transforming beautiful homes into masterpieces. From custom cabinetry to dramatic coffered ceilings, every element we create is built to elevate, impress, and endure.</p>
       <div className={style.serviceWrapper}>
       {services.map((service) => (
         <div key={service.id} >
