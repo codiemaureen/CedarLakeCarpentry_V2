a04f84068125587e9e4dcfa18de26e6a09a71d29
Codiemaureen <codiestover.cs@gmail.com>
2026-04-22 20:01:09 -0400

    Cleaning up footer

--- src/app/component/Footer.jsx
+++ src/app/component/Footer.jsx
@@ -3,9 +3,8 @@ import style from '../component/Footer.module.css';
 const Footer = () => {
   return (
     <div className={style.footerContainer}>
+      <a href='/'>Return to Home</a>
       <p>Website built by <a href="https://codiegroth.com/">Codie Groth</a></p>
-      <p>Header Image by Photo by <a href='https://www.pexels.com/photo/carpenter-using-circular-saw-for-precise-woodcutting-28518832/' alt='header-photo-cred'>Mario</a></p>
-      <p>Services Images by <a href='https://www.pexels.com/photo/couch-with-blue-pillows-in-a-living-room-5178068/'>Curtis</a></p>
     </div>
   )
 }
