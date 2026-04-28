33c926c991dc4c76cd2a555de1b9820ff25904f8
Codiemaureen <codiestover.cs@gmail.com>
2026-04-28 13:56:59 -0400

    Styling the trust section

--- src/app/component/TrustSection.jsx
+++ src/app/component/TrustSection.jsx
@@ -1,19 +1,20 @@
 import styles from "../component/TrustSection.module.css"
 function TrustSection() {
   return (
-   <section className={styles.trustSection}>
-     <h2 className={styles.heading}>
-       Trusted by Builders Across Metro Detroit
-     </h2>
+    <section className={styles.trustSection}>
+      <h2 className={styles.heading}>
+          Trusted by Leading Luxury Home Builders<br />
+          in Metro Detroit
+      </h2>
 
-     <div className={styles.logos}>
-       <span>Robertson Brothers Homes</span>
-       <span>Pulte Homes</span>
-       <span>Toll Brothers</span>
-       <span>MGM Homes</span>
-       <span>Saske Construction</span>
-     </div>
-   </section>
+      <div className={styles.logos}>
+        <span>Robertson Brothers Homes</span>
+        <span>Pulte Homes</span>
+        <span>Toll Brothers</span>
+        <span>MGM Homes</span>
+        <span>Saske Construction</span>
+      </div>
+    </section>
   )
 }
 
